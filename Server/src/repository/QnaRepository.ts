d5329065e2058dc1174e8b8a3713c5d37e53b2da
Kim_ByungChul <bcking92@gmail.com>
2021-02-02 00:37:23 +0900

    Update QnaRepository.ts

--- Server/src/repository/QnaRepository.ts
+++ Server/src/repository/QnaRepository.ts
@@ -1,44 +1,26 @@
 import { QNA } from "../model/Qna";
 import { Transaction } from "sequelize/types";
 
-<<<<<<< Updated upstream
-export class DeliLocationRepository {
-=======
 export class QnaRepository {
->>>>>>> Stashed changes
   constructor() {}
 
   insertQna(transaction:Transaction, qna:any) {
     return QNA.create({...qna}, {transaction:transaction})
   }
 
-<<<<<<< Updated upstream
-  selectQna(transaction:Transaction, qna:any) {
-    return QNA.findOne({ where: {QNA_ID: qna.QNA_ID} , transaction: transaction})
-  }
-
-  selectQnas(transaction:Transaction, page: number) {
-    return QNA.findAll({ offset: (page-1) * 30, limit: 30})  
-=======
   selectQna(transaction:Transaction, qnaId:number) {
     return QNA.findOne({ where: {QNA_ID: qnaId} , transaction: transaction})
   }
 
   selectQnas(transaction:Transaction, page:number) {
     return QNA.findAll({ offset: (page-1) * 30, limit: 30, transaction: transaction})  
->>>>>>> Stashed changes
   }
 
   updateQna(transaction:Transaction, qna:any) {
     return QNA.update({...qna}, {where: {QNA_ID: qna.QNA_ID}, transaction:transaction})
   }
 
-<<<<<<< Updated upstream
-  deleteQna(transaction:Transaction, qna:any) {
-    return QNA.destroy({where: {QNA_ID: qna.QNA_ID}, transaction:transaction})
-=======
   deleteQna(transaction:Transaction, qnaId:any) {
     return QNA.destroy({where: {QNA_ID: qnaId}, transaction:transaction})
->>>>>>> Stashed changes
   }
 }
